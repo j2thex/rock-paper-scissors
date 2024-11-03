3c6a46b15dfbb5ca0d6a2de155ef57f3bf2f8f4b
j2thex <jnamed@gmail.com>
2024-11-03 19:19:15 +0100

    Initial Commit

---
+++ ios-test-appTests/ios_test_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_test_appTests.swift
+//  ios-test-appTests
+//
+//  Created by Jeffrey Smith on 3/11/24.
+//
+
+import Testing
+@testable import ios_test_app
+
+struct ios_test_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
